e01ff5719bfb6f8f4e3ae3e94c414a1299b44ed4
Simon Fallnich <simon@fallnich.net>
2017-11-15 17:47:12 +0100

    Added getOwner to standard contract

--- src/blockchain/contracts/Standard.sol
+++ src/blockchain/contracts/Standard.sol
@@ -12,6 +12,14 @@ contract owned {
 	function owned() {
 		owner = msg.sender; // Store the address of the sender as owner
 	}
+
+	/**
+	 * Get the owner of the contract.
+	 * @returns {address} The address of the contract's owner
+	 */
+	function getOwner() constant public returns (address) {
+		return owner;
+	}
 	
 }
 
