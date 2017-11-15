961926c50a3110526c7dab14998a362d08ce6ff0
Simon Fallnich <simon@fallnich.net>
2017-11-15 17:08:14 +0100

    Refactored web3 config

--- src/middleware/server/config/web3.js
+++ src/middleware/server/config/web3.js
@@ -1,9 +1,12 @@
 // Import dependencies
 const web3 = require('web3')
 
+// Define values
+PROVIDER_URL = 'http://testrpc:8545'
+
 // Create instance
 const instance = new web3()
-instance.setProvider(new instance.providers.HttpProvider('http://testrpc:8545'))
+instance.setProvider(new instance.providers.HttpProvider(PROVIDER_URL))
 
 // Export module
 module.export = instance
