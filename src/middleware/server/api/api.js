2111c612c95c5411f3177ce0f807def02c2d5de6
Simon Fallnich <simon@fallnich.net>
2017-11-15 13:36:42 +0100

    Added documentation

--- src/middleware/server/api/api.js
+++ src/middleware/server/api/api.js
@@ -1,7 +1,13 @@
 // Import dependencies
 const router = require('express').Router()
 
-// Response functions
+/**
+ * Send a JSON response.
+ *
+ * @param {Object} res - The response object
+ * @param {Number} status - The status code for the response
+ * @param {Object} text - A JSON object which contains any additional data
+ */
 function response(res, status, text) {
 	var obj = {status: status}
 	if (text) obj.text = text
@@ -10,6 +16,13 @@ function response(res, status, text) {
 	res.end()
 }
 
+/**
+ * Send an error response.
+ *
+ * @param {Object} res - The response object
+ * @param {Number} status - The status code for the response
+ * @param {String} err - An error message which gets logged
+ */
 function error(res, status, err) {
 	console.log(err)
 	response(res, status)
