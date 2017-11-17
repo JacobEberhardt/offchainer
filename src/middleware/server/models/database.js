2a4d5f547a2e9d36bfddce0da450fcaad7c2d91c
Simon Fallnich <simon@fallnich.net>
2017-11-17 09:42:14 +0100

    Added basic database functionality

--- src/middleware/server/models/database.js
+++ src/middleware/server/models/database.js
@@ -1,2 +1,39 @@
 // Import dependecies
 const sqlize = require('sequelize')
+
+// Define scheme
+const db = global.db.define('offchainer',
+	{message: {type: sqlize.STRING}},
+	{timestamps: false}
+)
+
+// Define functions
+/**
+ * Store a given message in the database
+ *
+ * @param {String} message The given message
+ * @returns {Promise} A promise which depends on the database response
+ */
+function setMessage(message) {
+	return db.create({
+		message: message
+	})	
+}
+
+/**
+ * Get the message for a given ID
+ *
+ * @param {String} id The given ID
+ * @returns {Promise} A promise which depends on the database response
+ */
+function getMessage(id) {
+	return db.findOne({
+		where: {id: id}
+	})
+}
+
+// Export module
+module.exports = {
+	setMessage,
+	getMessage
+}
