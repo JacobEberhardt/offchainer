f894c4d08b13d070873d7088b3fc3f5210d595ca
Simon Fallnich <simon@fallnich.net>
2017-11-19 19:41:48 +0100

    Added check for postgres environment variables

--- src/middleware/server/config/postgresql.js
+++ src/middleware/server/config/postgresql.js
@@ -1,6 +1,11 @@
 // Import dependencies
 const Sequelize = require('sequelize')
 
+// Check environment variables
+if (process.env.PG_HOST === undefined) throw ValueError('Environment variable PG_HOST is not set')
+if (process.env.PG_PORT === undefined) throw ValueError('Environment variable PG_PORT is not set')
+if (process.env.PG_USER === undefined) throw ValueError('Environment variable PG_USER is not set')
+
 // Define values
 const PG_HOST = process.env.PG_HOST
 const PG_PORT = process.env.PG_PORT
