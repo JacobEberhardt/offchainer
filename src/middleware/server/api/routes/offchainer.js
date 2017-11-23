a09d724107f63989d016811c9b30905599cdc4db
Simon Fallnich <simon@fallnich.net>
2017-11-23 17:35:46 +0100

    Generalized database model

--- src/middleware/server/api/routes/offchainer.js
+++ src/middleware/server/api/routes/offchainer.js
@@ -1,12 +1,24 @@
 // Import dependencies
 const router = require('express').Router()
+const Sequelize = require('sequelize')
+const Database = require('../../models/database')
 const offchainer = require('../../models/offchainer')
-const db = require('../../models/database')
 const res = require('../../utils/response')
 
 // Set response functions
 const response = res.response
 const error = res.error
+
+// Initialize database connection
+const db = new Database(
+	'offchainer',
+	{
+		key: {type: Sequelize.STRING},
+		message: {type: Sequelize.STRING}
+	}
+)
+
+console.log(db)
 
 // Routes
 /**
@@ -28,7 +40,7 @@ router.post('/message', (req, res, next) => {
 	if (!offchainer.hasAddress()) return response(res, 400, 'Create a contract first') // Check if a contract was created
 	if (!req.body.message || typeof(req.body.message) != 'string') return response(res, 400, 'Invalid message')	// Check if the given message is valid
 	offchainer.setMessage(req.body.message)
-		.then(hash => db.setMessage(hash, req.body.message))
+		.then(hash => db.create({key: hash, message: req.body.message}))
 		.then(result => response(res, 200, {hash: result.key}))
 		.catch(err => error(res, 500, err))
 })
@@ -39,7 +51,7 @@ router.post('/message', (req, res, next) => {
 router.post('/verify', (req, res, next) => {
 	if (!offchainer.hasAddress()) return response(res, 400, 'Create a contract first') // Check if a contract was created
 	if (!req.body.hash || typeof(req.body.hash) != 'string') return response(res, 400, 'Invalid hash') // Check if a contract was created
-	db.getMessage(req.body.hash)
+	db.read({key: req.body.hash})
 		.then(result => offchainer.checkMessage(result.message))
 		.then(success => response(res, 200, {success: success}))
 		.catch(err => error(res, 500, err))
