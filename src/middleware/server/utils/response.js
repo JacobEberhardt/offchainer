441ea7d41920c6ffe73b331ff3d73536702961eb
Simon Fallnich <simon@fallnich.net>
2017-11-23 14:33:33 +0100

    Outsourced the response functions

---
+++ src/middleware/server/utils/response.js
@@ -0,0 +1,32 @@
+/**
+ * Send a JSON response.
+ *
+ * @param {Object} res The response object
+ * @param {Number} status The status code for the response
+ * @param {Object} content A JSON object which contains any additional data
+ */
+function response(res, status, content) {
+	var obj = {status: status}
+	if (content) obj.content = content
+	res.writeHead(status, {'Content-Type': 'application/json'})
+	res.write(JSON.stringify(obj))
+	res.end()
+}
+
+/**
+ * Send an error response.
+ *
+ * @param {Object} res The response object
+ * @param {Number} status The status code for the response
+ * @param {String} err An error message which gets logged
+ */
+function error(res, status, err) {
+	console.error(err.stack)
+	response(res, status)
+}
+
+// Export module
+module.exports = {
+	response,
+	error
+}
