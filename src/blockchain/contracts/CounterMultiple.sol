ac017f8ffbcd4f96029b0c34ab1dd4f27e12fb43
Thanh <tenhcong.thanhtuan@gmail.com>
2017-12-05 13:23:04 +0100

    #vrme: Adjust SC

--- src/blockchain/contracts/CounterMultiple.sol
+++ src/blockchain/contracts/CounterMultiple.sol
@@ -1,25 +1,24 @@
 // Required version
 pragma solidity ^0.4.17;
 
-contract Counter {
+contract CounterMultiple {
 
 	// Declare variables
-	bytes32 integrityHash; // The integrity hash of the counters array
 	mapping(uint => bytes32) integrityHashes; // The integrity hashes of the counters array
 
 	// Declare events
-	event RequestedCounterIncreaseEvent(bytes32 integrityHash);
 	event IntegrityCheckFailedEvent();
-	event CounterIncreasedEvent(uint8[4] counters);
-	event returnNewRootHash(bytes32 proof, uint newCounterValue);
-	event RequestSingleDataEvent(uint rowId, uint colId);
 	event IntegrityCheckFailedEvent2(bytes32 proof1,bytes32 proof2);
+	event ReturnNewRootHash(bytes32 proof, uint newCounterValue);
+	event RequestSingleDataEvent(uint rowId, uint colId);
+	
 
 	// Define public functions
 	/**
 	 * Create a new contract instance.
 	 */
 	function Counter() public {
+
 	}
 
 	/**
@@ -39,41 +38,11 @@ contract Counter {
 	/**
 	 * Request the increase of the counter with the given index.
 	 *
-	 * @param _index The index of the counter to increase (zero-based)
-	 */
-	function requestCounterIncrease(uint _index) public {
-		RequestedCounterIncreaseEvent(integrityHash);
-	}
-
-	/**
-	 * Request the increase of the counter with the given index.
-	 *
 	 * @param _rowId The index of the row of counter
 	 * @param _colId The index of the column of the counter to increase (zero-based)
 	 */
 	function requestSingleCounterIncrease(uint _rowId, uint _colId) public {
 		RequestSingleDataEvent(_rowId, _colId);
-	}
-
-	/**
-	 * Perform an increase of the counter with the given index.
-	 *
-	 * @param _counterValue value of the counter in int
-	 * @param _proof proof
-	 * @param _proofPosition proof positions
-	 */
-	function doCounterIncrease(uint8 _counterValue, bytes32[] _proof, uint[] _proofPosition) public {
-	    //Perform integrity check by reconstrcuting the merkle tree.
-	    bytes32 computedHash = _createTree(_counterValue, _proof, _proofPosition);
-
-        if(computedHash == integrityHash) {
-            uint8 newCounterValue = _counterValue + 1;
-            // get new roothash after increasing the counter
-            integrityHash = _createTree(newCounterValue, _proof, _proofPosition);
-            returnNewRootHash(integrityHash, newCounterValue);
-        } else {
-            IntegrityCheckFailedEvent();
-        }
 	}
 
 	/**
@@ -87,25 +56,14 @@ contract Counter {
 	    //Perform integrity check by reconstrcuting the merkle tree.
 	    bytes32 computedHash = _createTree(_counterValue, _proof, _proofPosition);
 
-        if(computedHash == integrityHashes[_rowId]) {
+        if (computedHash == integrityHashes[_rowId]) {
             uint8 newCounterValue = _counterValue + 1;
             // get new roothash after increasing the counter
             integrityHashes[_rowId] = _createTree(newCounterValue, _proof, _proofPosition);
-            returnNewRootHash(integrityHashes[_rowId], newCounterValue);
+            ReturnNewRootHash(integrityHashes[_rowId], newCounterValue);
         } else {
 			IntegrityCheckFailedEvent2(integrityHashes[_rowId], computedHash);
         }
-	}
-
-	// Define private functions
-	/**
-	 * Compute the hash of a given array of integers.
-	 *
-	 * @param _counters The array of integers to compute the hash for
-	 * @return The computed hash
-	 */
-	function _computeHash(uint8[4] _counters) private pure returns (bytes32 hash) {
-		return keccak256(_counters);
 	}
 
 	/**
@@ -118,7 +76,6 @@ contract Counter {
 	 */
 	function _createTree(uint8 _counterValue, bytes32[] _proof, uint[] _proofPosition) private returns (bytes32) {
 	    bytes32 computedHash = keccak256(_counterValue);
-
 	    for(uint8 i = 0; i < _proof.length; i++) {
 	        if(_proofPosition[i] == 0) {
 	            // if left
