b1a4f60345cde0be18f5f5ccb375a5cb79d7d37c
Simon Fallnich <simon@fallnich.net>
2017-12-11 13:19:42 +0100

    #vrmf: Replaced TestRPC by Ganache

--- src/blockchain/truffle.js
+++ src/blockchain/truffle.js
@@ -2,9 +2,9 @@ module.exports = {
 	networks: {
 		development: {
 			host: 'localhost',
-			port: 8545,
-			network_id: '*',
-			gas: 555500000
+			port: 7545,
+			network_id: '5777',
+			gas: 1000000000
 		}
 	} 
 }
