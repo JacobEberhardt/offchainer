5238ba0e26712747793b3e54accabe910ed34df7
Simon Fallnich <simon@fallnich.net>
2017-12-11 14:46:33 +0100

    #vrmf: Removed unsupported numbers from environment variables

--- src/middleware/server/config/web3.js
+++ src/middleware/server/config/web3.js
@@ -3,7 +3,8 @@ const web3 = require('web3')
 
 // Define values
 WEBTHREE_HOST = process.env.WEBTHREE_HOST || 'localhost'
-PROVIDER_URL = 'http://' + WEBTHREE_HOST + ':7545'
+WEBTHREE_PORT = process.env.WEBTHREE_PORT || '7545'
+PROVIDER_URL = 'http://' + WEBTHREE_HOST + ':' + WEBTHREE_PORT
 
 // Create instance
 var instance = new web3()
