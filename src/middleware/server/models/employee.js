483f446cd2b182698e9a2fdceb96e629f6fea04f
Thanh <tenhcong.thanhtuan@gmail.com>
2017-12-14 22:59:46 +0100

    #vmht: Implemented and finished employee pay raise functions

--- src/middleware/server/models/employee.js
+++ src/middleware/server/models/employee.js
@@ -8,10 +8,12 @@ const Database = require('./database')
 const Sequelize = require('sequelize')
 const MerkleTree = require('../utils/merkleTree')
 const sha3 = require('web3-utils').soliditySha3
+const transactions = require('../utils/transactions')
 
 // Define values
 CONTRACT_BUILD_FILE = '../../../blockchain/build/contracts/Employee.json'
 INITIAL_GAS = 4700000
+COLUMN_NAMES = ['first_name', 'last_name', 'start_date', 'department', 'salary']
 
 // Import contract data
 const contractData = JSON.parse(fs.readFileSync(path.join(__dirname, CONTRACT_BUILD_FILE)))
@@ -38,9 +40,6 @@ const db = new Database(
 		salary: { type: Sequelize.INTEGER }
 	}
 )
-
-// Define functions
-
 
 /**
  * Create a new contract instance.
@@ -78,10 +77,8 @@ function add(employee) {
 			employee.department,
 			employee.salary
 		].map(x => sha3(x))
-		console.log(leaves)
 		const tree = new MerkleTree(leaves, sha3)
 		const rootHash = tree.getRoot()
-		
 		db.create({
 			first_name: employee.firstName,
 			last_name: employee.lastName,
@@ -90,15 +87,19 @@ function add(employee) {
 			salary: employee.salary
 		})
 			.then(result => {
-				
-				promisify(contract.instance.add)({
+				return Promise.all([promisify(contract.instance.add)({
 					args: [
 						result.dataValues.id,
 						rootHash
 					]
-				})
-					.then(result => {
-						resolve(result)
+				}), result])
+					// .then(([result, previous]) => {
+					// 	console.log(result)
+					// 	console.log(previous)
+					// 	return Promise.all([transactions.waitForBlock(web3, result), previous])
+					// })
+					.then(([result, previous]) => {
+						resolve(previous)
 					})
 					.catch(err => reject(err))
 			})
@@ -116,6 +117,19 @@ function getAll() {
 }
 
 /**
+ * Return the root hash of an employee stored in the smart contract.
+ *
+ * @return {Promise} A promise that depends on the contract creation
+ */
+function getRootHash(index) {
+	return promisify(contract.instance.get)({
+		args: [
+			index
+		]
+	})
+}
+
+/**
  * Add a set of employees to the database and store the root hash of the data record into 
  * 
  * @param {Object[]} employees The set of employees to add
@@ -126,99 +140,132 @@ function importEmployees(employees) {
 }
 
 /**
- * Increase the salary of each affected employee.
+ * Increase the salary of a single employee.
  *
+ * @param {Object} employee The employee
+ * @returns {Promise} A promise that depends on the successful salary increase
+ */
+function increaseSalarySingleEmployee(employee) {
+	return new Promise ((resolve, reject) => {
+		// Error Handler
+		const handler = (error) => reject({
+			'id': employee.id,
+			'error': error
+		})
+
+		// Smart contract returns new values
+		events.watch(contract.instance.ReturnNewValues)
+			.then(result => {
+				// Wait for transaction to be mined, pass return values to next chain
+				return Promise.all([transactions.waitForBlock(web3, transactionHash), result])
+			})
+			.then(([result, previous])  => {
+				// Write new salary from previous chain results to database 
+				return Promise.all([db.update(
+					{id: previous.args.rowId.c[0]},
+					{
+						['salary']: previous.args.newSalary.c[0]
+					}
+				), previous])
+			})
+			.then(([result, previous]) => resolve(result))
+			.catch(([error, previous]) => {
+				if(error.code === "database") {
+					// Rollback in case database write failed
+					promisify(contract.instance.rollBack)({
+						args: [
+							previous.args.rowId.c[0],
+							previous.args.prevRootHash
+						]
+					})
+					reject({
+						'id' : employee.id,
+						'error' : error
+					})
+				} else {
+					reject({
+						'id' : employee.id,
+						'error' : error
+					})
+				}
+			})
+
+		// Integrity check of data record failed
+		events.watch(contract.instance.IntegrityCheckFailedEvent)
+			.then((result) => {
+				console.log('Integrity check failed.')
+				reject({
+					'id' : employee.id,
+					'error' : 'Integrity check failed.'
+				})
+			})
+		
+		// Position of salary in employee record (-1 because id is shifted)
+		const indexOfSalary = Object.keys(employee).indexOf('salary') - 1
+
+		// Create array of leaves from employee object and shift id
+		const leaves =  Object.values(employee)
+		leaves.shift()
+
+		// Construct merkle tree and get proof
+		const tree = new MerkleTree(leaves.map(x => sha3(x)), sha3)
+		const proof = tree.getProof(indexOfSalary)
+
+		// Increase salary request for single employee
+		promisify(contract.instance.increaseSalarySingleEmployee)({
+			args: [
+				employee.id,
+				employee.salary,
+				proof.proofData,
+				proof.proofPosition
+			]
+		})
+			.then(result => transactionHash = result)
+			.catch(handler)
+	})
+}
+
+/**
+ * Increase the salary of all affected employee. Affected employees are defined in a payraise contract.
+ * 
+ * @param {String} payRaiseContractAddress The address of the payraise contract
  * @returns {Promise} A promise that depends on the successful salary increase
  */
 function increaseSalary(payRaiseContractAddress) {
 	return new Promise ((resolve, reject) => {
-		// Define functions
 		const handler = (err) => reject(err)
-		const increaseSalary = promisify(contract.instance.increaseSalarySingleEmployee)
-		const revertRootHash = promisify(contract.instance.rollBack)
-
-		// Define variables
-		var newRootTransactionHash,
-			oldRootHash,
-			newRootHash,
-			newCounterValue
-
-		// Set event listeners
+		let finalResult = []
 
 		// Smart contract needs data
 		events.watch(contract.instance.RetrieveDataEvent)
-			.then(result => db.readAll({
-				department: result.args.department
-				// start_date: contract.rowId
-			}))
 			.then(result => {
-				console.log(result);
-				// const leaves = [
-				// 	result.counter_one,
-				// 	result.counter_two,
-				// 	result.counter_three,
-				// 	result.counter_four
-				// ]
-
-				// const tree = new MerkleTree(leaves.map(sha3), sha3)
-				// const proof = tree.getProof(index)
-
-				// return doCounterIncrease({
-				// 	args: [
-				// 		leaves[index],
-				// 		proof.proofData,
-				// 		proof.proofPosition,
-				// 		{gas: 300000}
-				// 	]
-				// })
-
-			})
-			// .then(result => newRootTransactionHash = result) // Store the transaction hash where a state is being changed
-			// .catch(handler)
-
-		// Smart contract returns new root hash
-		events.watch(contract.instance.ReturnNewValues)
-			.then(result => {
-				
-				// oldRootHash = result.args.prevRootHash
-				// newRootHash = result.args.newRootHash
-				// newCounterValue = result.args.newCounterValue.c[0]
-
-				// return transactions.waitForBlock(web3, newRootTransactionHash)
-
-			})
-			// .then(() => {
-
-			// 	const colName = COLUMN_NAMES[index]
-
-			// 	return db.update(
-			// 		{id: contract.rowId},
-			// 		{
-			// 			[colName]: newCounterValue,
-			// 			root_hash: newRootHash
-			// 		}
-			// 	)
-			// 		.catch(error => {
-			// 			revertRootHash({args: oldRootHash})
-			// 			reject(error)
-			// 		})
-
-			// })
-			// .then(result => resolve(result))
-			// .catch(handler)
-		// Given data failed the integrity check
-		events.watch(contract.instance.IntegrityCheckFailedEvent)
-			.then(() => reject('Integrity check failed.'))
-			.catch(handler)
-
-		// Request counter increase
-		promisify(contract.instance.requestIncreaseSalary)({
-					args: [
-						payRaiseContractAddress
-					]
+				let department = web3.toUtf8(result.args.department)
+				return db.readAll({
+					department: department
 				})
+			})
+			.then(result => {
+				console.log(result)
+				// Sequential promise chain
+				return result.reduce((promise, item) => {
+					return promise
+						.then((result) => {
+							return increaseSalarySingleEmployee(item.dataValues)
+						})
+						.then(result => finalResult.push(result[1][0]))
+						.catch(error => finalResult.push(error))
+				}, Promise.resolve());
+			})
+			.then(result => resolve(finalResult))
 			.catch(handler)
 
+		// Increase salary request for all employees, which are returned from the database
+		promisify(contract.instance.requestIncreaseSalary)({
+			args: [
+				payRaiseContractAddress
+			]
+		})
+			.catch(handler)
 	})
 }
 
@@ -245,6 +292,7 @@ function hasInstance() {
 module.exports = {
 	create,
 	add,
+	getRootHash,
 	getAll,
 	importEmployees,
 	increaseSalary,
