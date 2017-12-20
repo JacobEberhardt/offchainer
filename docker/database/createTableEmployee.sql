313f5af728726bce8b1cfed559b22363d9beb771
Simon Fallnich <simon@fallnich.net>
2017-12-20 22:54:23 +0100

    Revert "Merge branch '(#vmht)_Add_More_Complex_Use_Cases' of https://github.com/simonfall/offchainer into _#vrmf_Create_One_Proof_for_Multiple_Items"
    This reverts commit 304385b4dd2f1a296656dae2fc5e1d074e4bcc57, reversing
    changes made to ec6d1163a7ded0ddd6f5dcc88fd797de20a3a303.

--- docker/database/createTableEmployee.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employee (
-	id SERIAL UNIQUE PRIMARY KEY NOT NULL,
-	first_name TEXT,
-	last_name TEXT,
-	start_date TEXT,
-	department TEXT,
-	salary INTEGER
-);
--- src/middleware/server/api/api.js
+++ src/middleware/server/api/api.js
@@ -3,9 +3,6 @@ const router = require('express').Router()
 const res = require('../utils/response')
 const offchainer = require('./routes/offchainer')
 const counter = require('./routes/counter')
-const employee = require('./routes/employee')
-const payraise = require('./routes/payraise')
-const financials = require('./routes/financials')
 const counterMutiple = require('./routes/counterMultiple')
 const counterOnchain = require('./routes/counterOnchain')
 
@@ -15,9 +12,6 @@ const error = res.error
 // Routes
 router.use('/offchainer', offchainer)
 router.use('/counter', counter)
-router.use('/employee', employee)
-router.use('/payraise', payraise)
-router.use('/financials', financials)
 router.use('/counter-multiple', counterMutiple)
 router.use('/counterOnchain', counterOnchain)
 
