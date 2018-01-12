b3d79165b3b43403d5e5bcc7ad6449550720e9b4
Simon Fallnich <simon@fallnich.net>
2018-01-12 13:31:40 +0100

    Added employee and payraise contracts to automatic deployment

--- src/blockchain/migrations/2_deploy_contracts.js
+++ src/blockchain/migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 // Import dependencies
 const Offchainer = artifacts.require('Offchainer')
 const Counter = artifacts.require('Counter')
+const Employee = artifacts.require('Employee')
+const PayRaise = artifacts.require('PayRaise')
 
 // Export module
 module.exports = deployer => {
 	deployer.deploy(Offchainer)
 	deployer.deploy(Counter)
+	deployer.deploy(Employee)
+	deployer.deploy(PayRaise)
 }
