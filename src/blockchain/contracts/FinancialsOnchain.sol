be0e6dd1524d3cf58a25681da198c5848e91176c
Tarek Higazi <Tarek.higazi@gmail.com>
2018-01-17 17:17:06 +0100

    #x00q: removed small piece of code

--- src/blockchain/contracts/FinancialsOnchain.sol
+++ src/blockchain/contracts/FinancialsOnchain.sol
@@ -46,10 +46,7 @@ contract Financials {
 	function addRecordEntry( uint256 date, string company_name, string recording_date, 
 	uint256 total_sales, uint256 cogs, uint256 inventory_stock, uint256 cash_counter,
 	uint256 accounts_receivables, uint256 accounts_payable) public {
-	    // datesToHashes[now] = _rootHash;
-	    // RecordEntry memory recordEntry = RecordEntry(now, _rootHash);
-		// recordEntries[recordIndex] = recordEntry;
-		// recordIndex++;
+
 		
 		recordEntries[recordIndex] = RecordEntry(date, company_name, recording_date,
 		total_sales, cogs, inventory_stock, cash_counter, accounts_receivables, accounts_payable);
