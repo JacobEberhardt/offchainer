c609a82be377ff04d34499e702451aed24c2efe5
Unknown <patrick.friedrich@outlook.com>
2018-01-18 23:05:02 +0100

    #xa8u & #xa8x: Smart Contract rewritten
    needs to be finished: Merkle Proof

--- src/blockchain/contracts/Financials.sol
+++ src/blockchain/contracts/Financials.sol
@@ -6,15 +6,16 @@ contract Financials {
 	// Variables
 	address creator;
 	uint private recordIndex = 0;
-	mapping(uint => RecordEntry) recordEntries;
-	mapping(uint => bytes32) datesToHashes;
 
-	mapping(uint => bytes32) rootHashes;
+	//recordIndex counter mapping to root hashes of rows
+	mapping(uint => bytes32) recordIndexToRootHashes;
+	//recordIndex counter mapping to index in database
+	mapping(uint => uint) recordIndexToDatabaseIndex;
+	//index in database to mapping to root hashes of rows
+	mapping(uint => bytes32) databaseIndexToRootHashes;
 
-	struct RecordEntry {
-        uint date;
-        bytes32 rootHash;
-    }
+	bytes32 rootHashAllRows;
+	bytes32 concatenatedRows;
 
 	// Events
 	event IntegrityCheckCompletedEvent(string resultOfIntegrityCheck);
@@ -26,65 +27,103 @@ contract Financials {
 	 */
 	function Financials() public {
 	    creator = msg.sender;
+	    rootHashAllRows = 0;
+	    concatenatedRows = 0;
 	}
 
-	// Public functions
-	/**
-	 * Adds a new record entry to recordEntries mapping.
-	 * @param _index The index of the record entry
-	 * @param _rootHash The root hash of the merkle tree of the record entry
-	 */
-	function addRecordEntry(uint _index, bytes32 _rootHash) public {
-	    // datesToHashes[now] = _rootHash;
-	    // RecordEntry memory recordEntry = RecordEntry(now, _rootHash);
-		// recordEntries[recordIndex] = recordEntry;
-		// recordIndex++;
-		rootHashes[_index] = _rootHash;
-	}
-	/**
-	 * Return all records of entries stored in the SC. The record includes date and 
-	 * roothash of a row.
-	 */
-	function getAllRecordEntries() constant returns(RecordEntry[]) {
-	    RecordEntry[] memory currentRecordEntries = new RecordEntry[](recordIndex);
-	    for(uint i = 0; i < recordIndex; i++){
-	        currentRecordEntries[i] = recordEntries[i];
+
+	function checkRowRootHashSCIndex(uint scIndex, bytes32 rootHashToVerify) constant public returns(bool){
+	    if(recordIndexToRootHashes[scIndex] == rootHashToVerify){
+	        return true;
 	    }
-	    return currentRecordEntries;
-	}
-
-	/**
-	 * Return an array of all the roothashes stored in SC.
-	 */
-	function getAllRootHashes() constant returns(bytes32[]) {
-	    bytes32[] memory currentRootHashes = new bytes32[](recordIndex);
-	    for(uint i = 0; i < recordIndex; i++){
-	        currentRootHashes[i] = recordEntries[i].rootHash;
+	    else{
+	        return false;
 	    }
-	    return currentRootHashes;
 	}
 
-	/**
-	 * Return one record stored in the SC
-	 * @param indexOfRecord the index of the record
-	 */
-	function getRecordEntry(uint indexOfRecord) constant returns(RecordEntry) {
-	    return recordEntries[indexOfRecord];
+	function checkRowRootHashDatabaseIndex(uint dbIndex, bytes32 rootHashToVerify) constant public returns(bool){
+	    if(databaseIndexToRootHashes[dbIndex] == rootHashToVerify){
+	        return true;
+	    }
+	    else{
+	        return false;
+	    }
 	}
 
- 	/**
-	 * Returns one roothash of the chosen record
-	 * @param indexOfRecord the index of the record 
-	 */
-	function getRootHash(uint indexOfRecord) constant returns(bytes32) {
-	    return rootHashes[indexOfRecord];
+	function checkRowDataSCIndex(uint scIndex, uint[] rowDataToVerify) constant public returns(bool){
+	    //TODO: perform Merkle Hashing on rowDataToVerify
+	    bytes32 resultingRootHash = 0;
+	    if(recordIndexToRootHashes[scIndex] == resultingRootHash){
+	        return true;
+	    }
+	    else{
+	        return false;
+	    }
 	}
 
-	/**
-	 * Checks the integrity of a record, calls another event to display the result.
-	 */
-	function checkIntegrityOfRecord() constant{
-	    IntegrityCheckCompletedEvent("Integrity of Data was verified");
+	function checkRowDataDatabaseIndex(uint dbIndex, uint[] rowDataToVerify) constant public returns(bool){
+	    //TODO: perform Merkle Hashing on rowDataToVerify
+	    bytes32 resultingRootHash = 0;
+	    if(databaseIndexToRootHashes[dbIndex] == resultingRootHash){
+	        return true;
+	    }
+	    else{
+	        return false;
+	    }
 	}
 
+	function checkAllRows(bytes32[] roothashesToVerify) constant public returns(bool){
+	    for(uint i = 0; i < recordIndex; i++){
+	        if(recordIndexToRootHashes[i] != roothashesToVerify[i]){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+
+	function checkSeveralRowsSCIndex(uint[] scIndexesOfRowsToVerify, bytes32[] roothashesToVerify) constant public returns(bool){
+	    assert(scIndexesOfRowsToVerify.length == roothashesToVerify.length);
+	    for(uint i = 0; i < scIndexesOfRowsToVerify.length; i++){
+	        if(recordIndexToRootHashes[scIndexesOfRowsToVerify[i]] != roothashesToVerify[i]){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+
+	function checkSeveralRowsDatabaseIndex(uint[] dbIndexesOfRowsToVerify, bytes32[] roothashesToVerify) constant public returns(bool){
+	    assert(dbIndexesOfRowsToVerify.length == roothashesToVerify.length);
+	    for(uint i = 0; i < dbIndexesOfRowsToVerify.length; i++){
+	        if(databaseIndexToRootHashes[dbIndexesOfRowsToVerify[i]] != roothashesToVerify[i]){
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+
+	function append(uint dbIndex, bytes32 roothashToAppend) public returns(uint){
+	    recordIndexToRootHashes[recordIndex] = roothashToAppend;
+	    recordIndexToDatabaseIndex[recordIndex] = dbIndex;
+	    databaseIndexToRootHashes[dbIndex] = roothashToAppend;
+
+	    calcConcatHash(roothashToAppend);
+
+	    recordIndex ++;
+	    return (recordIndex - 1);
+	}
+
+	function calcConcatHash(bytes32 roothashToAppend) private {
+	    if(concatenatedRows != 0){
+	        //concatenate concatenatedRows and roothashToAppend
+	        //https://ethereum.stackexchange.com/questions/729/how-to-concatenate-strings-in-solidity
+	        //https://ethereum.stackexchange.com/questions/1081/how-to-concatenate-a-bytes32-array-to-a-string
+	        concatenatedRows = concatenatedRows & roothashToAppend; //not concatenated but added, should serve same cause
+	        rootHashAllRows = keccak256(concatenatedRows);
+	    }
+	    else{
+	        //first entry
+	        concatenatedRows = roothashToAppend;
+	        rootHashAllRows = keccak256(concatenatedRows);
+	    }
+	}
 }
