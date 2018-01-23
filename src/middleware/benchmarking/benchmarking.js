07103c586ea79c728e1e2079966aedb3701fb0cc
Kevin Marcel Styp-Rekowski <k.styp-rekowski@campus.tu-berlin.de>
2018-01-23 12:36:19 +0100

    #xa9c: Installed supertest, and tried to import server. Maybe we just import the routes and give the ip as a parameter...

--- src/middleware/benchmarking/benchmarking.js
+++ src/middleware/benchmarking/benchmarking.js
@@ -1,1 +1,10 @@
+const request = require('supertest')
+
+PG_HOST=database
+PG_PORT=5432
+PG_USER=postgres
+PG_DATABASE=offchainer
+const server = require('../server/server.js')
+const api = require('../api/api.js')
+
 console.log("I got called.")
