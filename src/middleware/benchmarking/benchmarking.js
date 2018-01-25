2dc41c201568e1a29d9e6b7d8fcb878d51ee8c3c
Kevin Marcel Styp-Rekowski <k.styp-rekowski@campus.tu-berlin.de>
2018-01-25 17:29:17 +0100

    #xa9c: Added Nested request Calls for Employee-On-Chain-use-Case, still need to add sended JSON-Data.

--- src/middleware/benchmarking/benchmarking.js
+++ src/middleware/benchmarking/benchmarking.js
@@ -11,3 +11,35 @@ const server = require('../server/server.js')
 //const api = require('../api/api.js')
 
 console.log("I got called.")
+
+console.log("server:")
+console.log(server)
+
+console.log("call create")
+request(server)
+  .post('/employeeOnchain/create')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .then( response => {
+    request(server)
+      .post('/employeeOnchain/add')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then( response => {
+        request(server)
+          .post('/employeeOnchain/increase-salary')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function(err, res) {
+            if (err) throw err;
+          });
+      })
+      /*.end(function(err, res) {
+        if (err) throw err;
+      });*/
+  })
+  /*.end(function(err, res) {
+    if (err) throw err;
+
+  })*/
+console.log("create called.")
