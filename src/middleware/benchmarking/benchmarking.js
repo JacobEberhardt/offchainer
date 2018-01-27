87ffab3313a3459d6e9b4cf68c7439b4644b69de
Kevin Marcel Styp-Rekowski <k.styp-rekowski@campus.tu-berlin.de>
2018-01-27 14:53:00 +0100

    #xa9c: Added JSon send files for propper functionality.

--- src/middleware/benchmarking/benchmarking.js
+++ src/middleware/benchmarking/benchmarking.js
@@ -12,8 +12,9 @@ const server = require('../server/server.js')
 
 console.log("I got called.")
 
-console.log("server:")
-console.log(server)
+//console.log("server:")
+//console.log(server)
+
 
 console.log("call create")
 request(server)
@@ -21,19 +22,55 @@ request(server)
   .expect('Content-Type', /json/)
   .expect(200)
   .then( response => {
-    request(server)
-      .post('/employeeOnchain/add')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then( response => {
-        request(server)
-          .post('/employeeOnchain/increase-salary')
+    sleep(2000).then(() => {
+      request(server)
+        .post('/employeeOnchain/add')
+        .send({
+        	"firstName" : "Adam",
+        	"lastName" : "Miller",
+        	"startDate" : 1515942162,
+        	"department" : "IT",
+        	"salary" : 2000
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then( response => {
+          request(server)
+          .post('/payraiseOnchain/create')
+          .send({
+          	"percentage" : 15,
+          	"department" : "IT",
+          	"fromStartDate" : 1515942162
+          })
           .expect('Content-Type', /json/)
           .expect(200)
-          .end(function(err, res) {
-            if (err) throw err;
-          });
-      })
+          .then( response => {
+            console.log("respone from payraise-create:")
+            console.log(response)
+            console.log("relevant parts: \n\n")
+            const payraiseAddress = response.body.content.address
+            console.log(payraiseAddress)
+            console.log("lol")
+            request(server)
+            .post('/employeeOnchain/increase-salary')
+            .send({
+            	"contractAddress": payraiseAddress
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+              if (err) throw err;
+              done()
+            });
+          })
+
+
+
+
+
+        })
+    })
+
       /*.end(function(err, res) {
         if (err) throw err;
       });*/
@@ -43,3 +80,8 @@ request(server)
 
   })*/
 console.log("create called.")
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
