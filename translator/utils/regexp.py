e243d98fda478cb939d64522c7ca773c2d3c0537
Simon Fallnich <simon@fallnich.net>
2018-02-04 16:03:23 +0100

    #xa91: Added regular expressions

---
+++ translator/utils/regexp.py
@@ -0,0 +1,8 @@
+# Import dependencies
+import re
+
+# Define expressions
+contract_name = re.compile('contract ([A-Za-z]+)[\s]*{[^\}]*}')
+function = re.compile('(function[\s]+([A-za-z]+)[^\{]+{(?:[^{}]*{[^}]*})*[^}]*})')
+state_variable = re.compile('(uint[\d]{0,3}|string|bool)(?:\[([\d]+)\])*[\s]+([A-Za-z]+)[\s]*;')
+comment = re.compile('(?:\/\/.*)|(?:\/\*[\S\s]*?\*\/)')
