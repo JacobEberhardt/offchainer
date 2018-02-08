3b07288b1836a5b0e2a2573578ad8ecfb284cbd1
Simon Fallnich <simon@fallnich.net>
2018-02-08 11:39:19 +0100

    #xa91: Added regular expressions for variables and functions

--- translator/utils/regexp.py
+++ translator/utils/regexp.py
@@ -3,6 +3,17 @@ import re
 
 # Define expressions
 contract_name = re.compile('contract ([A-Za-z]+)[\s]*{(?:[^{}]*{[^}]*})*[^}]*}')
-function = re.compile('(function[\s]+(?:[A-za-z]+)[^\{]+{(?:[^{}]*{[^}]*})*[^}]*})')
-state_variable = re.compile('(uint[\d]{0,3}|string|bool)(?:\[([\d]+)\])*[\s]+([A-Za-z]+)[\s]*;')
+function = re.compile('(function[\s]+[A-za-z]+[^\{]+{(?:[^{}]*{[^}]*})*[^}]*})')
+variable = re.compile('((?:uint[\d]{0,3}|string|bool)(?:\[([\d]+)\])*[\s]+(?:[A-Za-z]+)[\s]*;)')
 comment = re.compile('(?:\/\/.*)|(?:\/\*[\S\s]*?\*\/)')
+
+# Variable expressions
+variable_combined = re.compile('(uint[\d]{0,3}|string|bool)(?:\[([\d]+)\])*[\s]+([A-Za-z]+)[\s]*;')
+variable_used_template = '(?:\(|=)[^"\'\nA-Za-z]*{}'
+variable_changed_template = '{}(?:\[[^\]]+\])?[^\S\n]*[\+-\/]*='
+
+# Function expressions
+function_name = re.compile('function[\s]+([A-za-z]+)[\s]*\(')
+function_arguments = re.compile('function[\s]+[A-za-z]+[\s]*\(([^\)]*)\)')
+function_modifiers = re.compile('function[\s]+[A-za-z]+[\s]*\([^\)]*\)([^\{]*){')
+function_body = re.compile('function[\s]+[A-za-z]+[^\{]+{((?:[^{}]*{[^}]*})*[^}]*)}')
