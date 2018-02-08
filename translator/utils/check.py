fbbc0cbf5cd669c4851884a7f0485d815ff7f7cf
Simon Fallnich <simon@fallnich.net>
2018-02-08 09:38:12 +0100

    #xa91: Added check for Solidity contract validity

--- translator/utils/check.py
+++ translator/utils/check.py
@@ -1,3 +1,11 @@
+# Import dependencies
+from . import regexp as reg
+
 # Define functions
-def isContract(lines):
-    return True
+def isContract(content):
+
+    # Trivial check for contract syntax
+    if reg.contract_name.search(content) is None:
+        return False
+    else:
+        return True
